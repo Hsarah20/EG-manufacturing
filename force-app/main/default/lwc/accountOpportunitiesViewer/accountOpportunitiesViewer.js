8833f9bf3166da47e59d71191602b3504c0fa643
Hasnaa Dessagne <hasnaliod@gmail.com>
2024-11-14 15:50:24 +0100

    fact-amelioration du code et correction

--- force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
+++ force-app/main/default/lwc/accountOpportunitiesViewer/accountOpportunitiesViewer.js
@@ -1,29 +1,27 @@
 import { LightningElement, api, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 import { reduceErrors } from 'c/ldsUtils';
-import NAME from '@salesforce/schema/Opportunity.Name';
-import AMOUNT from '@salesforce/schema/Opportunity.Amount';
-import CLOSEDATE from '@salesforce/schema/Opportunity.CloseDate';
-import STAGENAME from '@salesforce/schema/Opportunity.StageName';
 import getOpportunities from '@salesforce/apex/AccountOpportunitiesController.getOpportunities';
 
+COLUMNS = [
+    { label: 'Nom Opportunité', fieldName: 'Name', type: 'text' },
+    { label: 'Montant', fieldName: 'Amount', type: 'currency' },
+    { label: 'Date de Clôture', fieldName: 'CloseDate', type: 'date', typeAttributes: { day: 'numeric', month: 'short', year: 'numeric' } },
+    { label: 'Phase', fieldName: 'StageName', type: 'text' }
+];
 export default class AccountOpportunitiesViewer extends LightningElement {
     @api recordId;
     opportunities;
     errors;
-    columns = [
-        { label: 'Nom Opportunité', fieldName: NAME.fieldApiName, type: 'text' },
-        { label: 'Montant', fieldName: AMOUNT.fieldApiName, type: 'currency' },
-        { label: 'Date de Clôture', fieldName: CLOSEDATE.fieldApiName, type: 'date' },
-        { label: 'Phase', fieldName: STAGENAME.fieldApiName, type: 'text' }
-    ];
+    columns = COLUMNS;
 
-    @wire(getOpportunities, { recordId: '$recordId' })
+    @wire(getOpportunities, { accountId: this.recordId })
     wiredOpportunities({ data, error }) {
         if (data) {
             this.opportunities = data;
             this.errors = undefined;
-            console.log("DATA OPPORTUNITIES " + this.opportunities);
+            console.log("DATA" + data)
+            console.log("OPPORTUNITIES " + this.opportunities);
         } else if (error) {
             console.error('Erreur de récupération des opportunités:', error);
             this.errors = reduceErrors(error);
