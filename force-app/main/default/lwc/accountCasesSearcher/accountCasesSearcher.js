b7b983b259ad43d7e4fe77a7ff8dddada4920be0
Hasnaa Dessagne <hasnaliod@gmail.com>
2024-11-14 15:54:50 +0100

    feat-add method findCasesBySubject in js && request in controller

--- force-app/main/default/lwc/accountCasesSearcher/accountCasesSearcher.js
+++ force-app/main/default/lwc/accountCasesSearcher/accountCasesSearcher.js
@@ -5,6 +5,7 @@ const COLUMNS = [
     { label: 'Sujet', fieldName: 'Subject', type: 'text' },
     { label: 'Statut', fieldName: 'Status', type: 'text' },
     { label: 'Priorité', fieldName: 'Priority', type: 'text' },
+    { label: 'Date de Création', fieldName: 'CreatedDate', type: 'date', typeAttributes: { day: 'numeric', month: 'short', year: 'numeric' } }
 ];
 
 export default class AccountCaseSearchComponent extends LightningElement {
@@ -19,13 +20,22 @@ export default class AccountCaseSearchComponent extends LightningElement {
     }
 
     handleSearch() {
+        if (!this.searchTerm.trim()) {
+            this.error = 'Veuillez entrer un terme de recherche.';
+            this.cases = undefined;
+            return;
+        }
         findCasesBySubject({ accountId: this.recordId, subjectSearchTerm: this.searchTerm })
             .then(result => {
-                this.cases = result;
-                this.error = undefined;
+                if (result.length > 0) {
+                    this.cases = result;
+                    this.error = undefined;
+                } else {
+                    this.error = 'Aucun cas trouvé pour le sujet donné.';
+                    this.cases = undefined;
+                }
             })
             .catch(error => {
-                //this.error = 'Une erreur est survenue lors de la recherche des cases.';
                 console.error('Erreur lors de la recherche des cases:', error);
                 this.error = 'Une erreur est survenue lors de la recherche des cases.';
                 this.cases = undefined;
